2bc4a85e9f175d30954d3775daaa1554ceff70ab
Tim <timgon032@mail.ru>
2023-03-21 22:14:35 +0400

    Update global App

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { AuthProvider } from "./components/AuthContext";
+import { BasketProvider } from './components/BasketContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <BasketProvider>
+          <App />
+        </BasketProvider>
       </AuthProvider> 
     </BrowserRouter>
   </React.StrictMode>
