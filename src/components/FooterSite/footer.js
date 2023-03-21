c1bae244bce25017668b1414cb22120f5cc18122
Tim <timgon032@mail.ru>
2023-03-21 22:09:31 +0400

    Adding Component Footer site

---
+++ src/components/FooterSite/footer.js
@@ -0,0 +1,48 @@
+import { useAuth } from "../AuthContext";
+import CustomLink from "../CustomLink/CustomLink";
+import './footer.css';
+
+const Footer = () => {
+    
+    const {
+        isLoggedIn
+    } = useAuth()
+
+    return(
+        <footer className="footer">
+            <div className="footer__wrap flex">
+                <div className="footer__logo">
+                    <img className="footer__logo--img" src="./img/logotype.svg" alt="logo"/>
+                </div>
+                <nav className="footer__nav">
+                    <CustomLink to="/basket">Корзина</CustomLink>
+                    
+                    {isLoggedIn ? 
+                        <CustomLink to="/cabinet">Личный кабинет</CustomLink>
+                        :
+                        <CustomLink to="/auth">Личный кабинет</CustomLink>    
+                    }
+
+                    {/* <CustomLink to="/link">Связаться с нами</CustomLink> */}
+                    
+                </nav>
+                <div className="footer__social">
+                    <ul className="footer__list--social lst">
+                        <li className="footer__vk item">
+                            <a href="https://vk.com"><img className="footer__vk--img" src="./img/vk.svg" alt="vk"/></a>
+                        </li>
+                        <li className="footer__telegram item">
+                            <a href="https://telegram.com"><img className="footer__telegram--img" src="./img/telegram.svg" alt="telegram"/></a>
+                        </li>
+                        <li className="footer__youtube item">
+                            <a href="https://youtube.com"><img className="footer__youtube--img" src="./img/Youtube.svg" alt="youtube"/></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className="footer__text flex">&#169; ООО Компьютерный магазин. Все права защищены. 2023г.</div>
+        </footer>
+    )
+}
+
+export default Footer;
