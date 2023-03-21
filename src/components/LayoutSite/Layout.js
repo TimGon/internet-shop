a855c546b40d68591ee1219b27714d758e54a68f
Tim <timgon032@mail.ru>
2023-03-21 22:12:13 +0400

    Adding Component Layout site

---
+++ src/components/LayoutSite/Layout.js
@@ -0,0 +1,18 @@
+
+import Footer from "../FooterSite/footer";
+import Header from "../HeaderSite/Header";
+import Main from "../MainSite/Main";
+
+
+
+const Layout = () => {
+    return (
+        <>
+            <Header/>            
+            <Main/>
+            <Footer/>
+        </>
+    )
+}
+
+export default Layout;
