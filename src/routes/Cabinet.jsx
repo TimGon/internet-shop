eab1cf1383ce3c43c66330c36a9c724c121c65fb
Tim <timgon032@mail.ru>
2023-03-21 22:04:40 +0400

    Updating the cabinet user

--- src/routes/Cabinet.jsx
+++ src/routes/Cabinet.jsx
@@ -1,34 +1,35 @@
 import { Link } from "react-router-dom";
-import CustomLink from "../CustomLink/CustomLink";
-import './footer.css';
+import { useAuth } from "../components/AuthContext";
 
-const Footer = () => {
-    
-    return(
-        <footer className="footer">
-            <div className="footer__wrap flex">
-                <div className="footer__logo">
-                    <img className="footer__logo--img" src="./img/logotype.svg" alt="logo"/>
+const Cabinet = () => {
+
+    const {
+        authUser
+    } = useAuth()
+
+    return (
+        <>
+            <div className="cabinet flex">
+                <h1 className="cabinet__title">Личный кабинет</h1>
+                <div className="cabinet__wrapp">
+                    <div className="cabinet__name">
+                        <span className="title">Имя пользователя:{authUser[0]}</span>
+                        
+                    </div>
+                    <div className="cabinet__mail">
+                        <span className="title">Почта пользователя:{authUser[2]}</span>
+                    </div>
+                    <div className="cabinet__phone">
+                        <span className="title">Телефон пользователя:{authUser[1]}</span>
+                    </div>
+                    <div className="cabinet__order">
+                        <Link className="order-link" to={"/order"}>Заказы</Link>
+                    </div>
                 </div>
-                <nav className="footer__nav">
-                    <CustomLink to="/basket">Корзина</CustomLink>
-                    <CustomLink to="/cabinet">Личный кабинет</CustomLink>
-                    <CustomLink to="/link">Связаться с нами</CustomLink>
-                    
-                </nav>
-                <div className="footer__social">
-                    <ul className="footer__list--social lst">
-                        <li className="footer__vk item">
-                            <a href="https://vk.com"><img className="footer__vk--img" src="./img/vk.svg" alt="vk"/></a>
-                        </li>
-                        <li className="footer__telegram item">
-                            <a href="https://telegram.com"><img className="footer__telegram--img" src="./img/telegram.svg" alt="telegram"/></a>
-                        </li>
-                        <li className="footer__youtube item">
-                            <a href="https://youtube.com"><img className="footer__youtube--img" src="./img/Youtube.svg" alt="youtube"/></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div className="footer__text flex">&#169; ООО Компьютерный магазин. Все права защищены. 2023г.</div>
-      
+            </div> 
+        </>
+        
+    )
+}
+
+export default Cabinet;
