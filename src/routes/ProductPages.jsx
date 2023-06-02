395dd73ceb9b3c0d3ecd547d0c178e094581dd6d
Tim <timgon032@mail.ru>
2023-06-02 22:14:57 +0400

    Updated product page

--- src/routes/ProductPages.jsx
+++ src/routes/ProductPages.jsx
@@ -1,124 +1,63 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { useBasket } from "../components/BasketContext";
-import { getProduct } from "../components/getFunc/function";
-
-const Product = () => {
+import { useLocation} from "react-router-dom";
+import { getProduct } from "../components/ApiFunction/function";
+import Product from "../components/Product/Product";
+import ErrorBoundary from "../components/Errors/Error";
+import ErrorMsg from "../components/Errors/ErrorMsg";
+import Filter from "../components/FilterProducts/Filter";
+import { useProduct } from "../components/Contexts/ProductContext";
+import { useAuth } from "../components/Contexts/AuthContext";
+import ProdCreate from "../components/Create/CreateProd";
+import Search from "../components/SearchProducts/Search";
+const Products = () => {
     
-    const { product } = useParams();
-    const [categ, setCateg] = useState(''),
-          [prod, setProd] = useState(''),
-          [stateUse, setStateUse] = useState(true); 
+    const location = useLocation();
+    const prod = location.pathname.split('/product/').join('');
 
-    const {
-        addBasket,
-        basket,
-        addCount,
-        removeCount
-    } = useBasket();
+    const [stateUse, setStateUse] = useState(true); 
+
+    const { 
+        product, 
+        setProduct, 
+        result, 
+        setResult,
+        categ, 
+        setCateg,
+    } = useProduct();
+
+    const { status } =useAuth();
 
     useEffect(()=> {
         if(stateUse) {
-            getProduct(product, setProd, setCateg);
+            getProduct(prod, setProduct, setCateg, setResult);
             setStateUse(false);
         } 
-    }, [stateUse, product])
-
-// const add = (ident, nameProd, costProd, image) => {
-
-//     if( ident === id && nameProd === title && costProd === cost && image === img) {
-//         setId(ident)
-//         setTitle(nameProd)
-//         setCost(costProd)
-//         setImg(image)
-//         setCount(count+1)
-//         console.log('Count True', count)
-//     } else {
-//         setId(ident)
-//         setTitle(nameProd)
-//         setCost(costProd)
-//         setImg(image)
-//         setCount(1)
-//     }
-
-//     console.log(id, title, cost, img);
-    
-// }
-
-console.log('category', categ, 'product', prod)
+    }, [stateUse, prod])
 
     return(
         <div className="main__container">
-                <h1 className="title">{categ[2] === product ? categ[1]: ''}</h1>
-                    {/* <div className="flex">
-                            <div className="right flex">
-                            <h2 className="title">Фильтр</h2>
-                            <div className="right__brands flex">
-                                <div className="brand">
-                                    <input type="checkbox" name="" id="" /> AirTone
-                                </div>
-                                    <div className="brand">
-                                        <input type="checkbox" name="" id="" /> Acer
-                                    </div>
-                                    <div className="brand">
-                                        <input type="checkbox" name="" id="" /> Asus
-                                    </div>
-                                    <div className="brand">
-                                        <input type="checkbox" name="" id="" /> HP
-                                    </div>
-                                    <div className="brand">
-                                        <input type="checkbox" name="" id="" /> Hyper
-                                    </div>
-                                    <div className="brand">
-                                        <input type="checkbox" name="" id="" /> Lenovo
-                                    </div>
-                                    <div className="brand">
-                                        <input type="checkbox" name="" id="" /> MSI
-                                    </div>
-                                
-                            </div>
-                            <div className="right__costs flex">
-                                От <input type="text" /> До <input type="text" />
-                            </div>
-                        </div>
-                    </div> */}
-                    <div className="center flex">
-                            {typeof(prod) !== typeof('') ? prod.map(elem => {
-                                return(
-                                    <div key={elem.id} className="product flex">
-                                        {elem.category === product ?
-                                            basket.findIndex(product => product.id === elem.id) > -1 ?
-                                                <div className="product flex">  
-                                                    <img className="product__img" src={`./img/${elem.img}.jpg`} alt={elem.img} />
-                                                    <h2 className="product__title">{elem.title_product}</h2>
-                                                    <p className="product__cost">Цена: {elem.cost_product} рублей</p>
-                                                    
-                                                    <div className="button">
-                                                        <button className="buy" onClick={() => removeCount(elem.id)}>-</button>
-                                                        <span className="product__count">{basket.map(item => item.id === elem.id ? item.count : '')}</span>
-                                                        <button className="buy" onClick={() => {addCount(elem.id)}}>+</button>
-                                                    </div>
-
-                                                </div>
-                                        :
-                                        <>
-                                            <img className="product__img" src={`./img/${elem.img}.jpg`} alt={elem.img} />
-                                            <h2 className="product__title">{elem.title_product}</h2>
-                                            <span className="product__cost">Цена: {elem.cost_product} рублей</span>
-                                            <button className="buy" onClick={()=> {addBasket(elem)}}>Купить</button>
-                                        </>
-                                            :
-                                            ''
-                                        }
-                                    </div>
-                                )
-                            }):
-                            <div>Пусто</div>
+            {status === 'admin' ?
+            <ProdCreate/>:''
+            }
+            <Search/> 
+            <div className="rigths flex">
+                <Filter/>
+                <ErrorBoundary ErrorComponent={ErrorMsg}>
+                    <div className="products">
+                        <h1 className="title">
+                            {categ[2] === prod ? categ[1]: ''}
+                        </h1>
+                        <div className="center flex">
+                            {product !== null && result !== null && product && result ? 
+                                <Product nameProd={result} nameCateg={prod}/>
+                                : ''
                             }
                         </div>
-            
+                    </div>
+                </ErrorBoundary>
+            </div>
         </div>
     )   
 }
 
-export default Product;
+export default Products;
