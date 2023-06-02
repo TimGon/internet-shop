07fcaa6791b19624b3b1ccaeff4645662d5ece92
Tim <timgon032@mail.ru>
2023-06-02 22:19:07 +0400

    Update global App

--- src/App.js
+++ src/App.js
@@ -11,37 +11,41 @@ import Favorite from "./routes/FavoritePage";
 import NotFoundPage from "./routes/Notfoundpage";
 // import SinglePage from "./components/SinglePage/SinglePage";
 // import Admin from "./routes/AdminPage";
-import { useAuth } from "./components/AuthContext";
+import { useAuth } from "./components/Contexts/AuthContext";
 
 // import { useEffect, useState } from "react";
 import Product from "./routes/ProductPages";
+import StateOrder from "./components/StateOrder/StateOrderPage";
+import User from "./components/UsersOrder/User";
+import AboutUs from "./components/OurInfo/Our";
 
 const App = () => {
 
-  const { 
-    isLoggedIn
-    } = useAuth();
+  const { isLoggedIn } = useAuth();
 
   return (
     <>
       <Routes>
-        
+      
           <Route path="/" element={<Layout/>}>
             <Route index element={<Home/>}/>
-            <Route path="/:product" element={<Product/>}/>
+            <Route path="/product/:name" element={<Product/>}/>
             <Route path="basket" element={<Basket/>}/>
             <Route path="order" element={<Order/>}/>
-            {isLoggedIn ? 
-              <Route path="cabinet" element={<Cabinet/>}/> 
+            {isLoggedIn ?
+              <>
+                <Route path="cabinet" element={<Cabinet/>}/> 
+                <Route path="orders" element={<StateOrder/>}/>
+                <Route path="/user/:id" element={<User/>}/>
+              </> 
               : ''
             }
-
             <Route path="auth" element={<Auth /> }/>
             <Route path="register" element={<Register/>}/>
-
             <Route path="favorite" element={<Favorite/>}/>
-            <Route path="notfoundpage" element={<NotFoundPage/>}/>
-          </Route>         
+            <Route path="about" element={<AboutUs/>}/>
+            <Route path="*" element={<NotFoundPage/>}/>
+          </Route>
       </Routes>      
     </>
 
