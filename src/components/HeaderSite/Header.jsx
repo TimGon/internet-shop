a74ee87d42146f991668cd842ed0991307ad2e4a
Tim <timgon032@mail.ru>
2023-06-02 21:56:07 +0400

    Update header site

--- src/components/HeaderSite/Header.jsx
+++ src/components/HeaderSite/Header.jsx
@@ -1,64 +1,70 @@
-import { Navigate } from "react-router-dom";
-import { useAuth } from "../AuthContext";
-import { useBasket } from "../BasketContext";
+// import { useLocation } from "react-router-dom";
+import { useAuth } from "../Contexts/AuthContext";
+import { useBasket } from "../Contexts/BasketContext";
 import CustomLink from "../CustomLink/CustomLink";
+
 import './header.css';
+// import Search from "../SearchProducts/Search";
 
 const Header = () => {
 
     const {
         isLoggedIn,
         setAuthUser,
-        setIsLoggedIn
+        setIsLoggedIn,
+        setStatus
     } = useAuth()
 
-    const {
-        basket
-    } = useBasket();
+    const { basket } = useBasket();
 
     // const location = useLocation();
 
     const stateBtn = () => {
         setIsLoggedIn(false);
         setAuthUser(null);
-        <Navigate to={'/auth'} replace={true}/>
+        setStatus(null);    
     }
-    // console.log(basket.map(item => item.count))
+    
     return(
     <header className="header">
         <div className="header__wrap flex">
             <div className="header__logotype">
-                <img className="logo" src="./img/logo.svg" alt="logo" />
+                <CustomLink className="hide__mobile" to="/" >
+                    <img className="logo" src='./img/logo.svg' alt="logo" />
+                </CustomLink>
             </div>
             <div className="header__wrap--right flex">
                 
-                {/* {location.pathname === '/register' || location.pathname === '/auth' ? '' 
-                    : 
-                    <div className="header__search">
-                        <input className="header__search--input" type="search" placeholder="Поиск..."/>
-                    </div>
+                {/* {
+                    location.pathname === '/product/block' ? <Search/>  
+                    : ''
                 } */}
 
                 <div className="header__nav flex">
-                    <CustomLink to="/" >Главная</CustomLink>
-                    <CustomLink to="/basket">
+                    <CustomLink className="hide__mobile" to="/basket">
                         <div className="header__basket flex">
                             <img className="header__basket--img" src="./img/basket.svg" alt="basket" />
-                            <span className="header__basket--text">Корзина({basket.length})</span>
+                            <span className="header__basket--text">
+                                Корзина({basket.length})
+                            </span>
                         </div>
                     </CustomLink>
-                    {/* <CustomLink to="/favorite">                
+                    {/* <CustomLink className="hide__mobile" to="/favorite">                
                         <div className="header__favorite flex">
                             <img className="header__favorite--img" src="./img/heart.svg" alt="hear" />
-                            <span className="header__favorite--text">Избранное</span>
+                            <span className="header__favorite--text">
+                            Избранное
+                            </span>
                         </div>
                     </CustomLink> */}
                     {isLoggedIn ?
                         <CustomLink to ="/cabinet">
-                            <div className="header__auth flex">
-                                <img className="header__auth--img" src="./img/auth.svg" alt="user" />
-                                <span onClick={stateBtn} className="header__auth--text">Выйти</span>
-                                
+                            <div onClick={() => stateBtn()} className="header__auth flex">
+                                <img className="header__auth--img" 
+                                src="./img/auth.svg" alt="user" />
+                                <span className="header__auth--text">
+                                    Выйти
+                                </span>
                             </div>
                         </CustomLink>
                         :
@@ -69,10 +75,9 @@ const Header = () => {
                             </div>
                         </CustomLink>
                     }
-
                 </div>
+                
             </div>
-            
         </div>
     </header>
     )
