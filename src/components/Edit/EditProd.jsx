15c40fca709f443d9955d1f136c9348a46639db4
Tim <timgon032@mail.ru>
2023-06-02 21:52:45 +0400

    Adding new package Edit

---
+++ src/components/Edit/EditProd.jsx
@@ -0,0 +1,119 @@
+import { useEffect, useState } from "react";
+
+import { blurHandler, checkDscrProd, checkImg, checkNumber, checkProdName } from "../CheckForm/Checking";
+import { ErrorBlock } from "../Errors/ErrorBlock";
+import {updateProd } from "../ApiFunction/function";
+
+const EditProd = 
+   ({id, title, descr, cost, categ, brand, nameImg, setActive, setState}) => {
+   const [nameProd, setNameProd] = useState(''),
+         [dscrProd, setDscrProd] = useState(''),
+         [costProd, setCostProd] = useState(''),
+         [brandProd, setBrandProd] = useState(''),
+         [nameImgProd, setNameImgProd] = useState(''),
+         [categProd, setCategProd] = useState('');
+
+   const [nameProdErr, setNameProdErr] = useState
+      ('Поле с названием продукта не может быть пустым!'),
+         [dscrErr, setDscrErr] = useState
+      ('Поле с описанием не может быть пустым'),
+         [costErr, setCostErr] = useState
+      ('Поле со стоимостью не может быть пустым'),
+         [brandErr, setBrandErr] = useState
+      ('Поле с брендом не может быть пустым'),
+         [nameImgErr, setNameImgErr] = useState
+      ('Поле с названием изображения не может быть пустым!'),
+         [categProdErr, setCategProdErr] = useState
+      ('Поле с категорией не может быть пустым');
+
+   const [nameProdDirty, setNameProdDirty] = useState(false),
+         [dscrProdDirty, setDscrProdDirty] = useState(false),
+         [costProdDirty, setCostProdDirty] = useState(false),
+         [brandProdDirty, setBrandProdDirty] = useState(false),
+         [nameImgProdDirty, setNameImgProdDirty] = useState(false),
+         [categProdDirty, setCategProdDirty] = useState(false);
+
+   const [valid, setValid] = useState(false)
+   
+   useEffect(()=>{
+      if(nameProdErr || dscrErr || costErr || brandErr || nameImgErr || categProdErr) {
+         setValid(false);
+      } else {
+         setValid(true);
+      }
+   },[nameProdErr, dscrErr, costErr, brandErr, nameImgErr, categProdErr])
+
+   return(
+      <>
+         <div className="modal__title flex">
+            <h1>{title}:Edit</h1>
+            <button className="cross" onClick={()=> setActive(false)}/>
+         </div>
+         <form className="modal__form">
+            <div className="form-block">
+               <label>ID</label>
+               <input type="text" className="input-form" name="idProd" value={id} disabled/>
+            </div>
+            <div className="form-block">               
+               {ErrorBlock(nameProdDirty, nameProdErr)}
+               <label>Название товара</label>
+               <input type="text" className="input-form" name="titleProd" 
+               value={nameProd} placeholder={title} 
+               onChange={(e)=> checkProdName(e, setNameProd, setNameProdErr)} onBlur={(e) => blurHandler(e, setNameProdDirty)} 
+               />
+            </div>
+            <div className="form-block">               
+               {ErrorBlock(dscrProdDirty, dscrErr)}
+               <label>Описание товара</label>
+               <textarea className="input-form textarea-prod" type="text" name="descrProd" 
+               value={dscrProd} placeholder={descr} 
+               onChange={(e)=> checkDscrProd(e, setDscrProd, setDscrErr)} onBlur={(e) => blurHandler(e, setDscrProdDirty)}
+               />
+            </div>
+            <div className="form-block">
+               {ErrorBlock(costProdDirty, costErr)}
+               <label>Стоимость товара</label>
+               <input type="text" className="input-form" name="costProd" 
+               value={costProd} placeholder={cost} 
+               onChange={(e)=> checkNumber(e, setCostProd, setCostErr)} onBlur={(e) => blurHandler(e, setCostProdDirty)} 
+               />
+            </div>
+            <div className="form-block">
+               {ErrorBlock(brandProdDirty, brandErr)}
+               <label>Бренд товара</label>
+               <input type="text" className="input-form" name="brandProd" 
+               value={brandProd} placeholder={brand} 
+               onChange={(e)=> checkImg(e, setBrandProd, setBrandErr)} onBlur={(e) => blurHandler(e, setBrandProdDirty)} 
+               />
+            </div>
+            <div className="form-block">               
+               {ErrorBlock(nameImgProdDirty, nameImgErr)}
+               <label>Название изображение товара</label>
+               <input type="text" className="input-form" name="imgProd" 
+               value={nameImgProd} placeholder={nameImg} 
+               onChange={(e)=> checkImg(e, setNameImgProd, setNameImgErr)} onBlur={(e) => blurHandler(e, setNameImgProdDirty)} 
+               />
+            </div>
+
+            <div className="form-block">
+               {ErrorBlock(categProdDirty, categProdErr)}
+               <label>Категория товара</label>
+               <input type="text" className="input-form" name="categ" 
+               value={categProd} placeholder={categ} 
+               onChange={(e)=> checkImg(e, setCategProd, setCategProdErr)} onBlur={(e) => blurHandler(e, setCategProdDirty)} 
+               />
+            </div>
+
+            <button disabled={!valid} type="button" className="btn"
+               onClick={() => {
+                  updateProd(id, nameProd, dscrProd, costProd, categProd,  brandProd,  nameImgProd, setNameProd, setDscrProd, setCostProd, setCategProd, setBrandProd ,setNameImgProd, setActive, setState);
+               }}
+            >Изменить товар
+            </button>
+         </form>
+      </>
+      
+   )
+}
+
+export default EditProd;
