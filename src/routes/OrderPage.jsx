1871b5cf35b9a950198ae20e5a5ca2dfb394aa22
Tim <timgon032@mail.ru>
2023-06-02 22:13:57 +0400

    Updated information about a successful order

--- src/routes/OrderPage.jsx
+++ src/routes/OrderPage.jsx
@@ -1,41 +1,40 @@
 import { Link } from "react-router-dom";
-import { useAuth } from "../components/AuthContext";
-import { useBasket } from "../components/BasketContext";
+import { useAuth } from "../components/Contexts/AuthContext";
+import { useBasket } from "../components/Contexts/BasketContext";
+import { useOrder } from "../components/Contexts/OrderContext";
+import { getOrder, getOrders } from "../components/ApiFunction/function";
 
 const Order = () => {
+    
+    const { basket } = useBasket();
 
-    const {
-        basket,
-        setBasket
-    } = useBasket();
+    const { authUser, status } = useAuth();
 
-
-    const {
-        authUser,
-    } = useAuth();
-
-    const basketDel = () => {
-        setBasket(null);
-    }
+    const { 
+        setOrder
+    } = useOrder();
 
     return(
-        <>
-        {basket ?
-            <div className="order flex">
-                <span className="order__id"> Id заказа:{Math.floor(Math.random() * 10)}</span>
-                <h1 className="order__title">Квитанция оплаты</h1>
-                <p className="success">
-                    Спасибо, {authUser[0]}, за оформленный заказ!
-                    Мы свяжемся с вами по почте!
-                </p>
-                
-                <Link className="success__color" onClick={()=> basketDel()} to={'/'}>Перейти на главную</Link>
-            </div>
-            :
-            <div className="err">Заказ не оформлен!</div>  
-        }
-
-        </>
+        <div className="order__container">
+            {basket ?
+                <div className="order flex">
+                    <h1 className="order__title">Информация по заказу</h1>
+                    <span className="order__id">
+                        Ваш номер заказа указан в списке заказах!
+                    </span>
+                    <p className="success">
+                        Спасибо, {authUser[0]}, за оформленный заказ!
+                        C вами свяжется курьер или оператор сайта для уточнения об доставке.
+                    </p>
+                    
+                    <Link className="success__color" to={'/orders'} 
+                        onClick={()=> status === 'admin'? getOrder(setOrder):  getOrders(authUser[0], setOrder)}
+                    >Перейти к заказам</Link>
+                </div>
+                :
+                <div className="err">Заказ не оформлен!</div>  
+            }
+        </div>
     
     )
 
